ec60881f897a9b489e44d7a36624ebedb568b09a
Keet Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2017-12-26 20:13:59 +0530

    Added proper noun classificaiton

--- Capitalization.py
+++ Capitalization.py
@@ -1,3 +1,5 @@
+import string
+
 import nltk
 
 nltk.download('punkt')
@@ -11,14 +13,37 @@ def sentence_capitalizer(text):
 
     # for each sentence in the text, capitalize the first word
     for i in range(0, len(sentences)):
-        sentences[i] = sentences[i].strip()[0].capialize()
+        sentences[i] = sentences[i].strip().capitalize()
 
-    return sentences.join(sentences, " ")
+    return " ".join(sentences)
+
+"""This function will capitalize the proper nouns"""
+def proper_noun_capitalizer(text):
+    tokens = nltk.word_tokenize(text)
+    tagged = nltk.pos_tag(tokens)
+    for tag in tagged:
+        if tag[1] == "NNP" or tag[1] == "NNPS":
+            tag[0] = tag[0].capitalize()
+
+    return "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in tokens]).strip()
+
 
 """This function will be the main function of the program"""
 def main_function():
-    input_text = input()
-    sentence_capitalizer(input_text)
+    input_file = open("files/input_text.txt", 'r')
+    input_text = input_file.read()
+
+    print("Input Text is: " + input_text)
+
+    #convert it to lower case
+    input_text = input_text.lower()
+
+    output_text = sentence_capitalizer(input_text)
+    output_text = proper_noun_capitalizer(output_text)
+
+    print("Output Text = " + output_text)
+    input_file.close()
+
 
 
 # run the main function
